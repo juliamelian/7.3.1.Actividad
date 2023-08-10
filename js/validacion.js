57af434af3b7394bd82705294948b72e4ecd264c
Cristian-000 <fcristian480@gmail.com>
2023-08-10 18:25:24 -0300

    agregamos js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,37 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const registroBtn = document.getElementById("regBtn");
+registroBtn.addEventListener("click", validarRegistro);
+
+
+function validarRegistro() {
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const terminos = document.getElementById("terminos").checked;
+
+
+    if (
+        nombre.trim() === "" ||
+        apellido.trim() === "" ||
+        email.trim() === "" ||
+        password1.trim() === "" ||
+        password2.trim() === "" ||
+        !terminos
+    ) {
+        showAlertError();
+        return;
+    }
+
+    if (password1 !== password2) {
+        showAlertError();
+        return;
+    }
+
+    showAlertSuccess();
+}
+
